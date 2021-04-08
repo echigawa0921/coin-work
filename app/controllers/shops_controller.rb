be108492e1f6cde4ba06d3c16e58028a2afe5209
echigawa0921 <yuki.echigawa@di-v.co.jp>
2021-04-08 20:10:40 +0900

    Update shops_controller.rb

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -26,3 +26,31 @@ class ShopsController < ApplicationController
                                      :genre,:area,:image)
       end
 end
+class ShopsController < ApplicationController
+  def index
+      @shops = Shop.all
+  end
+  def new
+      @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.create(shop_params)
+    if @shop.save
+      redirect_to controller: :shops, action: :index
+    else
+      render "new"
+    end
+  end
+
+  def show
+      @shop = Shop.find(params[:id])
+  end
+  
+    private
+    def shop_params
+      params.require(:shop).permit(:name, :title, :content,:open_hour,:regular_holiday,:telephone,
+                                   :address,:nearest_station,:capacity,:private_room,:wifi,:power_supply,
+                                   :genre,:area,:image)
+    end
+end
