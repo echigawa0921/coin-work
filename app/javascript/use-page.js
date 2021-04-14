eaf323ed510775758815cfe51ffae7d88bc0ac7b
echigawa0921 <yuki.echigawa@di-v.co.jp>
2021-04-14 17:46:24 +0900

    Update use-page.js

--- app/javascript/use-page.js
+++ app/javascript/use-page.js
@@ -12,3 +12,17 @@
 		}			
 	});
 });
+(function(){
+	(".accordion li a").on("click", function() {
+		(this).next().slideToggle();	
+		// activeが存在する場合
+		if ((this).children(".accordion_icon").hasClass('active')) {			
+			// activeを削除
+			(this).children(".accordion_icon").removeClass('active');				
+		}
+		else {
+			// activeを追加
+			(this).children(".accordion_icon").addClass('active');			
+		}			
+	});
+});
