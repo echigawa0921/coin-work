e8513e878bf0227e718d52541e653b2944ebe2cb
echigawa0921 <yuki.echigawa@di-v.co.jp>
2021-04-20 22:30:03 +0900

    Update index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,8 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
