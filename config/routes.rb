c845c9d551b9d5744fed2982cb845e6f1787b6e5
echigawa0921 <yuki.echigawa@di-v.co.jp>
2021-04-22 23:01:02 +0900

    ジャンル・エリア遷移文

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   get "/shops/list"
+
+   get "/shops/area_osaka"
+   get "/shops/area_hyogo"
+   get "/shops/area_kyoto"
+
+   get "/shops/genre_fastfood"
+   get "/shops/genre_cafe"
+   get "/shops/genre_manga"
+   get "/shops/genre_karaoke"
+   get "/shops/genre_space"
+
   root to: 'shops#index'
   resources :shops
 end
