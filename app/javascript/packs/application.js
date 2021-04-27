07e7ef170f55a8c0015079b4d7b2529b1301071d
echigawa0921 <yuki.echigawa@di-v.co.jp>
2021-04-27 23:56:30 +0900

    javascript修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("use-page.js")
-require("jquery.use-page.js")
 require("bootstrap");
 
 
